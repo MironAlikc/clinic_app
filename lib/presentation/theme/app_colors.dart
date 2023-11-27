ea1373c94eb1022654210d837739c041d02266df
Aleksandr Myroniuk <miron.alikc@gmail.com>
2023-11-27 07:55:08 -0800

    first commit

---
+++ lib/presentation/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color black = Colors.black;
+  static const Color fontsColor = Color(0xFF333333);
+  static const Color buttonColor = Color(0xff4A86CC);
+  static const Color whith = Colors.white;
+}
